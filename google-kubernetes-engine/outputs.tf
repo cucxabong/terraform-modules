9c62f1088547c23663cf57b46a37ee802aef92cb
Quan Hoang <hdquan@protonmail.com>
2023-09-27 21:01:16 +0100

    output network

--- google-kubernetes-engine/outputs.tf
+++ google-kubernetes-engine/outputs.tf
@@ -5,3 +5,7 @@ output "endpoint" {
 output "ca_certificate" {
   value = google_container_cluster.this.master_auth[0].cluster_ca_certificate
 }
+
+output "network" {
+  value = google_compute_network.this
+}
